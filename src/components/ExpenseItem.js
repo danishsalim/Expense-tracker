c6a7c978a174fc23839d3852925653bf17c169ad
Danish <danishsalim321@gmail.com>
2023-12-06 20:54:52 +0530

    added heading and hardcoded list of items

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+
+
+const ExpenseItem = () => {
+  return (
+    <>
+    <h2>ExpenseItem</h2>
+        <ul>
+            <li>
+            Food Rs 10
+            </li>
+            <li>Petrol Rs 100</li>
+            <li>Movies Rs 200</li> 
+        </ul> 
+    </>
+    
+  )
+}
+export default ExpenseItem
