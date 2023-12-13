64e7559d8aa4fb17f4ddf66253b8f2ce21b18bd2
Danish <danishsalim321@gmail.com>
2023-12-14 02:35:08 +0530

    added clickHandlers functionality to ExpenseDetail compo for updating title and amount

--- src/components/Expenses/ExpenseDetail.js
+++ src/components/Expenses/ExpenseDetail.js
@@ -1,13 +1,21 @@
+import { useState } from 'react'
 import {expenses} from '../../App'
 import './ExpenseDetail.css'
  const ExpenseDetail = (props) => {
+    const [title,setTitle] = useState(props.title);
+    const [amount,setAmount] = useState(props.amount);
     const clickHandler=()=>{
         console.log("clicked")
+        setTitle("updated")
     }
     const deleteHandler=(e,idx)=>{
         // console.log("del "+idx)
         // console.log(e.target.parentElement)
         e.target.parentElement.classList.add('hide')
+    }
+    const changeAmountHandler =()=>
+    {
+        setAmount('100$')
     }
   return (
     <>
@@ -15,12 +23,13 @@ import './ExpenseDetail.css'
             {props.LocationOfExpenditure}
         </div>
         <div className='expense-item__description'>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
         </div>
         <div className='expense-item__price '>
-            ${props.amount}
+            ${amount}
         </div>
         <button onClick={clickHandler}>change title</button>
+        <button onClick={changeAmountHandler}>change price to 100$</button>
         <button onClick={(e)=>deleteHandler(e,props.id)}>Delete</button>
     </>
   )
