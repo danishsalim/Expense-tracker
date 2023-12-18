09cb25a04b7fb1d92c4309c17153c7e781932776
Danish <danishsalim321@gmail.com>
2023-12-18 14:47:23 +0530

    condition rendering on expenses.js . if no item found a message is shown on screen else the item is shown.

--- src/App.js
+++ src/App.js
@@ -20,13 +20,13 @@ import Expenses from "./components/Expenses/Expenses";
     id:'e3' ,
     title: 'Car Insurance',
     amount: '294.1',
-    date:new Date(2019,12,9) ,
+    date:new Date(2019,11,30) ,//(y,m,d)
   },
   {
     id: 'e4' ,
     title: 'New Desk',
     amount: '450',
-    date:new Date(2019,12,7) ,
+    date:new Date(2019,12,7) , //In JavaScript, the month parameter in the Date constructor is zero-based, meaning January is 0, February is 1, and so on. Therefore, when you create a Date object with new Date(2019, 12, 7), you are actually creating a date for the 13th month of 2019, which rolls over to January 2020.
   }
 ];
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,7 +12,38 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
   
-  const filteredExpense = props.items.filter((expense)=>expense.date.getFullYear().toString()===filteredYear)
+  const filteredExpense = props.items.filter((expense)=>
+                  {
+                  return expense.date.getFullYear().toString()=== filteredYear 
+                  }
+            )
+  let expensesContent = <p>No Expense Found</p>
+  if(filteredExpense.length===1)
+  {
+    expensesContent = filteredExpense.map((expense,idx) => (
+                    <div key={idx}> 
+                      <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                      />
+                      <p> Only single Expense here. Please add more...</p>
+                     </div>   
+                   ))
+  }
+  else if(filteredExpense.length>0)
+  {
+    expensesContent = filteredExpense.map((expense) => (
+                      <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                      />
+                      ))
+  }
+  
 
   return (
     <div>
@@ -21,16 +52,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {
-      filteredExpense.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))
-      }
+      {expensesContent}
     </Card>
   </div>
   );
